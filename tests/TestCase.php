23d7c8330f9a46189eeaea7960358c59b7cee0ba
SerafimArts <nesk@xakep.ru>
2023-02-01 15:38:24 +0000

    Rename github token to ACCESS_TOKEN

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Helix\Http\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * @group http
+ */
+abstract class TestCase extends BaseTestCase
+{
+}
